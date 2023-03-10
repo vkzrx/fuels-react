17e5c8c07637dbd0b443438724735c30d608a6be
0xYami <0xYami@proton.me>
2023-03-10 20:10:28 +0100

    add coins example

---
+++ examples/accounts/coins/src/App.tsx
@@ -0,0 +1,20 @@
+import { useCoins } from 'fuels-react';
+
+function App() {
+  const coins = useCoins({
+    address: 'fuel1wc9hhur34dywx3cvkynhaegw8yyyfcd646fp9qaln720w9jw6xzsjz755w',
+  });
+
+  if (coins.isLoading) return <div>Fetching coins...</div>;
+  if (coins.isError) return <div>Fetching coins has failed</div>;
+  return (
+    <>
+      <div>Status: {coins.status}</div>
+      <div>Found: {coins.data?.length}</div>
+    </>
+  );
+}
+
+
+export default App;
+
