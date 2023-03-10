247a031e1b851a32fad6c6ff542d3e741f905ab5
0xYami <0xYami@proton.me>
2023-03-10 20:37:38 +0100

    add connect example

---
+++ examples/accounts/connect/src/App.tsx
@@ -0,0 +1,20 @@
+import { useConnect } from 'fuels-react';
+
+function App() {
+  const { status, connect, isConnected } = useConnect({
+    onSuccess: () => {
+      alert('Successfully connected');
+    },
+  });
+
+  if (isConnected) return <div>Already connected</div>;
+  return (
+    <>
+      <div>Status: {status}</div>
+      <button onClick={connect}>Connect</button>
+    </>
+  );
+}
+
+
+export default App;
